aa776237b0f629d826a6171414666218d8e23213
Esa Firman <esafirm21@gmail.com>
2016-11-02 14:24:01 +0700

    Change Hawk to 2.0.1, Change BuildTools to 25.0.0, Change Target and Compile SDK version to 25

--- app/src/main/java/com/incendiary/androidboilerplate/data/local/Preferences.java
+++ app/src/main/java/com/incendiary/androidboilerplate/data/local/Preferences.java
@@ -2,9 +2,7 @@ package com.incendiary.androidboilerplate.data.local;
 
 import android.app.Application;
 import com.orhanobut.hawk.Hawk;
-import com.orhanobut.hawk.HawkBuilder;
-import com.orhanobut.hawk.LogLevel;
-import com.incendiary.androidboilerplate.BuildConfig;
+import com.orhanobut.hawk.NoEncryption;
 
 public class Preferences {
 
@@ -21,11 +19,7 @@ public class Preferences {
 
   public static void setup(Application application) {
     Hawk.init(application)
-        .setEncryptionMethod(HawkBuilder.EncryptionMethod.MEDIUM)
-        .setLogLevel(BuildConfig.DEBUG
-            ? LogLevel.FULL
-            : LogLevel.NONE)
-        .buildRx()
-        .subscribe();
+        .setEncryption(new NoEncryption())
+        .build();
   }
 }
